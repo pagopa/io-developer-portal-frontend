7c5df9592e7277d30437fb87ff5b27ee8881f88b
Gianluca Esposito <g@esposi.to>
2018-07-19 10:31:38 +0200

    Add upsert for PouchDB

--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,48 @@
 import { get, post } from "./api";
 
+const tryAndPut = (db, doc) => {
+  return db.put(doc).then(
+    res => {
+      return {
+        updated: true,
+        rev: res.rev,
+        id: doc._id
+      };
+    },
+    err => {
+      if (err.status !== 409) {
+        throw err;
+      }
+      return upsert(db, doc._id, doc);
+    }
+  );
+};
+
+const upsert = (db, docId, newDoc) => {
+  if (typeof docId !== "string") {
+    throw new Error("doc id is required");
+  }
+
+  return db
+    .get(docId)
+    .catch(err => {
+      if (err.status !== 404) {
+        throw err;
+      }
+      return {};
+    })
+    .then(doc => {
+      // the user might change the _rev, so save it for posterity
+      const docRev = doc._rev;
+
+      // users aren't allowed to modify these values,
+      // so reset them here
+      newDoc._id = docId;
+      newDoc._rev = docRev;
+      return tryAndPut(db, newDoc);
+    });
+};
+
 const contactGetAndPersist = async ({ db, code, batchId }) => {
   let profile = await get({ path: `profiles/${code}` });
 
@@ -9,12 +52,13 @@ const contactGetAndPersist = async ({ db, code, batchId }) => {
     profile = { sender_allowed: null, status: profile.status };
   }
 
-  return db.put({
+  const newDoc = {
     ...profile,
-    _id: code,
     type: "contact",
     batchId
-  });
+  };
+
+  return upsert(db, code, newDoc);
 };
 
 module.exports.contactGetAndPersist = contactGetAndPersist;
