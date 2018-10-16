f52ec475c961166aff2f8e3610f3ae36bb23bbf5
Spinelli Danilo <danilo.spinelli@agid.gov.it>
2018-10-16 10:28:20 +0200

    fix package.json for deploy

--- deploy.cmd
+++ deploy.cmd
@@ -102,7 +102,7 @@ IF EXIST "%DEPLOYMENT_TARGET%\package.json" (
   pushd "%DEPLOYMENT_TARGET%"
   call :ExecuteCmd !NPM_CMD! install
   IF !ERRORLEVEL! NEQ 0 goto error
-  call :ExecuteCmd !NPM_CMD! build
+  call :ExecuteCmd !NPM_CMD! run build
   IF !ERRORLEVEL! NEQ 0 goto error
   popd
 )
