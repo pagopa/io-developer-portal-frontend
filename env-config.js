f43c4225813b0495fd8e2b5db9fd795b93b2c6ae
danilo.spinelli <danilo.spinelli@pagopa.it>
2020-03-24 21:12:11 +0100

    fix: revert changes

--- env-config.js
+++ env-config.js
@@ -1,8 +1,3 @@
 window._env_ = {
-  IO_DEVELOPER_PORTAL_PORT: "80",
-  IO_DEVELOPER_PORTAL_PUBLIC_PATH: "/",
-  IO_DEVELOPER_PORTAL_APIM_BASE_URL:
-    "https://io-dev-apim-01.azure-api.net/api/v1",
-  IO_DEVELOPER_PORTAL_BASE_URL: "/",
-  IO_DEVELOPER_PORTAL_BACKEND: "http://localhost:3000"
+  // To be completed by env.sh
 };
