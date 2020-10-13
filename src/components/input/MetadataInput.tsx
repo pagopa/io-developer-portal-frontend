18f3c5cf92ecf5a76752bac6526972b8b4d8c25f
Simone Infante <simone.infante@pagopa.it>
2020-10-13 12:55:09 +0200

    refactoring MetadataInput component

--- src/components/input/MetadataInput.tsx
+++ src/components/input/MetadataInput.tsx
@@ -1,4 +1,4 @@
-import React, { ChangeEvent, Component } from "react";
+import React, { ChangeEvent } from "react";
 
 import { Service } from "io-functions-commons/dist/generated/definitions/Service";
 import { ServiceScopeEnum } from "io-functions-commons/dist/generated/definitions/ServiceScope";
@@ -14,60 +14,56 @@ type OwnProps = {
 
 type Props = WithNamespaces & OwnProps;
 
-class MetadataInput extends Component<Props, never> {
-  public render() {
-    const {
-      service: { service_metadata },
-      onChangeText,
-      onChangeSelect,
-      isApiAdmin,
-      t
-    } = this.props;
-
-    return isApiAdmin && service_metadata ? (
-      // All input text Metadata (except 'scope' that is an enumeration)
+const MetadataInput = ({
+  service: { service_metadata },
+  onChangeText,
+  onChangeSelect,
+  isApiAdmin,
+  t
+}: Props) => {
+  return isApiAdmin && service_metadata ? (
+    // All input text Metadata (except 'scope' that is an enumeration)
+    <div>
+      {Object.keys(service_metadata)
+        .filter(k => k !== "scope")
+        .map((k, i) => (
+          <div key={i}>
+            <label className="m-0">{t(k)}</label>
+            <input
+              name={k}
+              type="text"
+              defaultValue={Reflect.get(service_metadata, k)}
+              onChange={onChangeText}
+              className="mb-4"
+            />
+          </div>
+        ))}
       <div>
-        {Object.keys(service_metadata)
-          .filter(k => k !== "scope")
-          .map((k, i) => (
-            <div key={i}>
-              <label className="m-0">{t(k)}</label>
-              <input
-                name={k}
-                type="text"
-                defaultValue={Reflect.get(service_metadata, k)}
-                onChange={onChangeText}
-                className="mb-4"
-              />
-            </div>
-          ))}
-        <div>
-          <label className="m-0">{t("scope")} </label>
-          <select
-            name="scope"
-            value={service_metadata.scope}
-            className="mb-4"
-            onChange={onChangeSelect}
+        <label className="m-0">{t("scope")} </label>
+        <select
+          name="scope"
+          value={service_metadata.scope}
+          className="mb-4"
+          onChange={onChangeSelect}
+        >
+          <option
+            aria-selected="true"
+            key={ServiceScopeEnum.NATIONAL}
+            value={ServiceScopeEnum.NATIONAL}
           >
-            <option
-              aria-selected="true"
-              key={ServiceScopeEnum.NATIONAL}
-              value={ServiceScopeEnum.NATIONAL}
-            >
-              {ServiceScopeEnum.NATIONAL}
-            </option>
-            <option
-              aria-selected="true"
-              key={ServiceScopeEnum.LOCAL}
-              value={ServiceScopeEnum.LOCAL}
-            >
-              {ServiceScopeEnum.LOCAL}
-            </option>
-          </select>
-        </div>
+            {ServiceScopeEnum.NATIONAL}
+          </option>
+          <option
+            aria-selected="true"
+            key={ServiceScopeEnum.LOCAL}
+            value={ServiceScopeEnum.LOCAL}
+          >
+            {ServiceScopeEnum.LOCAL}
+          </option>
+        </select>
       </div>
-    ) : null;
-  }
-}
+    </div>
+  ) : null;
+};
 
 export default withNamespaces("service")(MetadataInput);
