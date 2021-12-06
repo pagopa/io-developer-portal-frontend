95e6477ec7f74a33761c45c6bf6024642fdfc148
Emanuele De Cupis <ema.decu@gmail.com>
2021-12-06 15:08:34 +0100

    refactor logout flow

--- src/utils/session/index.ts
+++ src/utils/session/index.ts
@@ -1,6 +1,18 @@
 import { MsalConfig } from "../../../generated/definitions/backend/MsalConfig";
+import { getUserAgentApplication } from "./msal";
 
 /**
  * The shape of configuration for the curretn session strategy
  */
 export type SessionConfig = MsalConfig; // will include more kinds in the future
+
+/**
+ * Clear and invalidates current session data
+ *
+ * @param configuration
+ * @returns
+ */
+export const logout = (configuration: SessionConfig): void => {
+  sessionStorage.clear();
+  return getUserAgentApplication(configuration).logout();
+};
