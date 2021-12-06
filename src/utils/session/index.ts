7351104e688ddfe23ab365def7d535b5547a9e42
Emanuele De Cupis <ema.decu@gmail.com>
2021-12-06 15:05:49 +0100

    add single entrypoint module for session handling flows

---
+++ src/utils/session/index.ts
@@ -0,0 +1,6 @@
+import { MsalConfig } from "../../../generated/definitions/backend/MsalConfig";
+
+/**
+ * The shape of configuration for the curretn session strategy
+ */
+export type SessionConfig = MsalConfig; // will include more kinds in the future
