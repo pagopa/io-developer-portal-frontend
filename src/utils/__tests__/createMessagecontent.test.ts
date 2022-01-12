12344d6da77c9b42cde075c1ee44b24bbfad0bab
LoFrance <lorenzo.franceschini@pagopa.it>
2022-01-12 13:39:30 +0100

    Fix lint error, replace text from IT to EN

--- src/utils/__tests__/createMessagecontent.test.ts
+++ src/utils/__tests__/createMessagecontent.test.ts
@@ -2,9 +2,9 @@ import { createMessageContent } from "../operations";
 
 const messageData = {
   message: {
-    subject: "Questo è un messaggio di prova",
+    subject: "A test subject",
     markdown:
-      "Questo è markdown, deve essere almeno di 80 caratteri altrimenti hai un errore!!"
+      "This is the markdown, it should be at least 80 characters long otherwise you'll got an error."
   },
   dueDate: "22/10/2021, 18:00",
   amount: "123",
@@ -21,42 +21,59 @@ describe("Create Message Content", () => {
     const data = mockMessageData();
     const res = createMessageContent(data);
     expect(res).not.toBeNull();
+    expect(res).toEqual({
+      markdown:
+        "This is the markdown, it should be at least 80 characters long otherwise you'll got an error.",
+      subject: "A test subject",
+      due_date: new Date("2021-10-22T16:00:00.000Z"),
+      payment_data: {
+        amount: 123,
+        notice_number: "000111222333444555",
+        invalid_after_due_date: true
+      }
+    });
   });
   it("should create a message without payment_data", () => {
     const data = mockMessageData.mockImplementationOnce(() => ({
       message: {
-        subject: "Questo è un messaggio di prova",
+        subject: "A test subject",
         markdown:
-          "Questo è markdown, deve essere almeno di 80 caratteri altrimenti hai un errore!!"
+          "This is the markdown, it should be at least 80 characters long otherwise you'll got an error."
       },
       dueDateFormat: "DD/MM/YYYY, HH:mm"
     }))();
     const res = createMessageContent(data);
     expect(res).not.toBeNull();
+    expect(res).toEqual({
+      markdown:
+        "This is the markdown, it should be at least 80 characters long otherwise you'll got an error.",
+      subject: "A test subject",
+      due_date: undefined
+    });
   });
   it("should throw 'Wrong parameters format' when subject is lesser than 10 characters", () => {
-    const data = mockMessageData.mockImplementationOnce(() => ({
+    const data = {
+      ...messageData,
       message: {
         subject: "Errato",
         markdown:
-          "Questo è markdown, deve essere almeno di 80 caratteri altrimenti hai un errore!!"
-      },
-      dueDateFormat: "DD/MM/YYYY, HH:mm"
-    }))();
+          "This is the markdown, it should be at least 80 characters long otherwise you'll got an error."
+      }
+    };
 
     expect(() => createMessageContent(data)).toThrowError(
       "Wrong parameters format"
     );
   });
+  // tslint:disable-next-line: no-identical-functions
   it("should throw 'Wrong parameters format' when message is lesser than 80 characters", () => {
-    const data = mockMessageData.mockImplementationOnce(() => ({
+    const data = {
+      ...messageData,
       message: {
-        subject: "Questo è un messaggio di prova",
-        markdown:
-          "Questo è markdown, di meno di 80 caratteri e deve andare in errore!!"
-      },
-      dueDateFormat: "DD/MM/YYYY, HH:mm"
-    }))();
+        subject: "A test subject",
+        markdown: "This is the markdown, it lesser than 80 characters long."
+      }
+    };
 
     expect(() => createMessageContent(data)).toThrowError(
       "Wrong parameters format"
