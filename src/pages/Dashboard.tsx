bbe41ff9eeac1c06afae4435ffea330183e9bb61
Emanuele De Cupis <ema.decu@gmail.com>
2022-02-28 16:20:47 +0100

    show summary bon into dashboard

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -12,12 +12,19 @@ import { RouteComponentProps } from "react-router";
 
 import "./Dashboard.css";
 
+import SummaryBox from "../components/subscription-migrations/SummaryBox";
 import {
   MessageDocument,
   MessagePostAndPersistResult
 } from "../utils/operations";
 import { ContactDocument } from "../workers/getProfile";
 import { TemplateDocument } from "./Message";
+
+import { get } from "lodash";
+import { PublicConfig } from "../../generated/definitions/backend/PublicConfig";
+import { getFromBackend } from "../utils/backend";
+import ff from "../utils/feature-flags";
+import { SelfCareSessionConfig } from "../utils/session/selfcare";
 
 type Props = RouteComponentProps<
   {},
@@ -26,7 +33,18 @@ type Props = RouteComponentProps<
 > &
   WithNamespaces;
 
-class Dashboard extends Component<Props, never> {
+type DashboardState = {
+  applicationConfig: PublicConfig;
+};
+
+class Dashboard extends Component<Props, DashboardState> {
+  public async componentDidMount() {
+    const applicationConfig = await getFromBackend<PublicConfig>({
+      path: "configuration"
+    });
+    this.setState({ applicationConfig });
+  }
+
   private getCard<T = {}>(docs: ReadonlyArray<T>, cardTextKey: string) {
     const { t } = this.props;
     return (
@@ -40,35 +58,45 @@ class Dashboard extends Component<Props, never> {
   }
   public render() {
     const { location } = this.props;
-
+    const applicationConfig = get(this.state, "applicationConfig");
     return (
-      <section className="d-flex">
-        <section className="position-fixed dashboard--notifications-container">
-          {location.state &&
-            Array.isArray(location.state) &&
-            location.state.map(info => {
-              return <Notification key={info._id} info={info} />;
-            })}
+      <>
+        <section className="d-flex">
+          <section className="position-fixed dashboard--notifications-container">
+            {location.state &&
+              Array.isArray(location.state) &&
+              location.state.map(info => {
+                return <Notification key={info._id} info={info} />;
+              })}
+          </section>
+          <Find<TemplateDocument>
+            selector={{
+              type: "template"
+            }}
+            render={({ docs }) => this.getCard(docs, "templates")}
+          />
+          <Find<MessageDocument>
+            selector={{
+              type: "message"
+            }}
+            render={({ docs }) => this.getCard(docs, "messages")}
+          />
+          <Find<ContactDocument>
+            selector={{
+              type: "contact"
+            }}
+            render={({ docs }) => this.getCard(docs, "contacts")}
+          />
         </section>
-        <Find<TemplateDocument>
-          selector={{
-            type: "template"
-          }}
-          render={({ docs }) => this.getCard(docs, "templates")}
-        />
-        <Find<MessageDocument>
-          selector={{
-            type: "message"
-          }}
-          render={({ docs }) => this.getCard(docs, "messages")}
-        />
-        <Find<ContactDocument>
-          selector={{
-            type: "contact"
-          }}
-          render={({ docs }) => this.getCard(docs, "contacts")}
-        />
-      </section>
+        {ff("SUBSCRIPTION_MIGRATIONS_ENABLED") &&
+          SelfCareSessionConfig.is(applicationConfig) && (
+            <section className="d-flex">
+              <div className="m-3 p-3 card">
+                <SummaryBox onSubmitHandler={() => console.log("noop")} />
+              </div>
+            </section>
+          )}
+      </>
     );
   }
 }
