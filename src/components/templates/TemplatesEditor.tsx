bdcc980fb5ed2f25ac6c65d39247c8cd21cce00f
Nazar Chekalin <37152374+Infectedmachine@users.noreply.github.com>
2022-05-03 14:47:09 +0200

    Fix linter (#215)

--- src/components/templates/TemplatesEditor.tsx
+++ src/components/templates/TemplatesEditor.tsx
@@ -39,7 +39,7 @@ class TemplatesEditor extends Component<Props, never> {
     const { t } = this.props;
 
     return (
-      <section className="d-flex flex-column" style={{ height: "600px"}}>
+      <section className="d-flex flex-column" style={{ height: "600px" }}>
         <Input
           className="font-weight-normal"
           type="text"
