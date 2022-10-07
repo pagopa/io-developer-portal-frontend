ce5f8447ce179c8b35b45d278e261110f385cec9
Emanuele De Cupis <ema.decu@gmail.com>
2022-10-07 23:22:26 +0200

    fix

--- env.sh
+++ env.sh
@@ -8,20 +8,11 @@ touch ./env-config.js
 echo "window._env_ = {" >> ./env-config.js
 
 # Loop on environment variables prefixed with
-# io_onboarding_pa and add them to env-config.js
-for io_developer_portal_var in $(env | grep -i io_developer_portal); do
-    varname=$(printf '%s\n' "$io_developer_portal_var" | sed -e 's/=.*//')
-    varvalue=$(printf '%s\n' "$io_developer_portal_var" | sed -e 's/^[^=]*=//')
-    debug=$(echo"$io_developer_portal_var")
-
-    echo "  $varname: \"$varvalue\"," >> ./env-config.js
-    echo "DEBUG: $io_developer_portal_var, $varname: \"$varvalue\" - \"$debug\""
-done
-
-echo "}" >> ./env-config.js
-
+# io_developer_portal and add them to env-config.js
 while IFS='=' read -r -d '' k v; do
     if [[ ${k,,} == io_developer_portal* ]]; then
-        printf "'%s'='%s'\n" "$k" "$v"
+        echo "  $k: \"$v\"," >> ./env-config.js
     fi
 done < <(env -0)
+
+echo "}" >> ./env-config.js
