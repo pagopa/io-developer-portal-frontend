7a0f9a3c3243a8bbe75a819d1793d3be0e447ec3
Giuseppe Di Pinto <giuseppeantonio.dipinto@pagopa.it>
2022-12-29 13:20:29 +0100

    [#IOPAE-104] add subscriptions pagination

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -31,6 +31,8 @@ import {
   ValidService
 } from "../utils/service";
 
+import "./Profile.css";
+
 const getMail = (email: string) =>
   email && email !== "" ? atob(email) : undefined;
 
@@ -109,6 +111,9 @@ const SubscriptionService = ({
   ) : null;
 };
 
+// max number of subscriptions loaded for each interaction (pagination)
+const subscriptionsLimit: number = 20;
+
 type Props = RouteComponentProps<{ email: string }> & WithNamespaces;
 
 function isString<T>(value: T | string): value is string {
@@ -143,6 +148,9 @@ type ProfileState = {
   onConfirmOperation: () => void;
   serviceState: { [serviceId: string]: string };
   showModal: boolean;
+  subscriptionsOffset: number;
+  hasMoreSubscriptions: boolean;
+  areSubscriptionsLoading: boolean,
 };
 
 class Profile extends Component<Props, ProfileState> {
@@ -162,7 +170,10 @@ class Profile extends Component<Props, ProfileState> {
     keyDisplay: {},
     isConfirmationOpen: false,
     onConfirmOperation: () => undefined,
-    showModal: false
+    showModal: false,
+    subscriptionsOffset: 0,
+    hasMoreSubscriptions: true,
+    areSubscriptionsLoading: true
   };
 
   public onAddSubscription = async () => {
@@ -269,11 +280,17 @@ class Profile extends Component<Props, ProfileState> {
       this.setState(this.getSubscriptionDefaults(applicationConfig));
     }
 
-    // load all user's subscriptions
+    // load user's subscriptions (paginated)
+    this.loadUserSubscriptions(this.state.subscriptionsOffset, email);
+  }
+
+  private async loadUserSubscriptions(offset: number, email?: string) {
+    this.setState({ areSubscriptionsLoading: true });
+    
     const userSubscriptions: SubscriptionCollection = await getFromBackend<
       SubscriptionCollection
     >({
-      path: "subscriptions" + (email ? "/" + encodeURIComponent(email) : "")
+      path: "subscriptions" + (email ? "/" + encodeURIComponent(email) : "") + `?offset=${offset}&limit=${subscriptionsLimit}`
     });
 
     const userSubscriptionsObj = Object.keys(userSubscriptions).reduce<
@@ -293,7 +310,10 @@ class Profile extends Component<Props, ProfileState> {
     }, {});
 
     this.setState({
-      userSubscriptions: userSubscriptionsObj
+      userSubscriptions: {...this.state.userSubscriptions, ...userSubscriptionsObj},
+      subscriptionsOffset: offset,
+      hasMoreSubscriptions: userSubscriptions['nextLink' as keyof typeof userSubscriptions] !== undefined,
+      areSubscriptionsLoading: false
     });
 
     // load all services related to the user's subscriptions
@@ -651,6 +671,26 @@ class Profile extends Component<Props, ProfileState> {
             },
             []
           )}
+        </div>
+
+        <div className="row">
+          <div className="col-md-12 text-center">
+            <button
+              onClick={() => {
+                this.loadUserSubscriptions(this.state.subscriptionsOffset + subscriptionsLimit);
+              }}
+              hidden={!this.state.hasMoreSubscriptions || (this.state.areSubscriptionsLoading && this.state.hasMoreSubscriptions)}
+              className="btn btn-secondary btn-lg btn-block"
+            >
+              {t("load_more_services")}
+            </button>
+            <div className={this.state.areSubscriptionsLoading ? 'progress profile--services-progess' : 'profile--services-progess-hidden'} 
+              style={this.state.areSubscriptionsLoading ?  {height: '56px'} : undefined}>
+              <div className="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" aria-valuenow={100} aria-valuemin={0} aria-valuemax={100} style={{width: '100%'}}>
+                {t("loading_services")}
+              </div>
+            </div>
+          </div>
         </div>
 
         <Confirmation
