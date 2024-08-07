f9d248ffa50474b4c46521539fe4d555562f6111
Mario Mupo <mario.mupo@pagopa.it>
2024-08-07 15:23:23 +0200

    feat: added identity and some configurations

---
+++ infra/identity/prod/locals.tf
@@ -0,0 +1,19 @@
+locals {
+  prefix    = "io"
+  env_short = "p"
+  env       = "prod"
+  location  = "italynorth"
+  project   = "${local.prefix}-${local.env_short}"
+  domain    = "devportal-frontend"
+
+  repo_name = "io-developer-portal-frontend"
+
+  tags = {
+    CostCenter     = "TS310 - PAGAMENTI & SERVIZI"
+    CreatedBy      = "Terraform"
+    Environment    = "Prod"
+    Owner          = "IO"
+    ManagementTeam = "IO Enti & Servizi"
+    Source         = "https://github.com/pagopa/io-developer-portal-frontend/blob/master/infra/identity/prod"
+  }
+}
