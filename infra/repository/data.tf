8e1b68ba5a1201f9e3006c01bce3da7c22ca9942
Matteo Anselmi <matteo.anselmi@bitrock.it>
2026-03-31 15:36:01 +0200

    remove unused data

--- infra/repository/data.tf
+++ infra/repository/data.tf
@@ -8,11 +8,6 @@ data "azurerm_user_assigned_identity" "identity_prod_cd" {
   resource_group_name = local.identity_resource_group_name
 }
 
-data "azurerm_user_assigned_identity" "identity_prod_tls_cd" {
-  name                = "${local.project}-devportal-frontend-github-prod-tls-cd-identity"
-  resource_group_name = local.identity_resource_group_name
-}
-
 data "github_organization_teams" "all" {
   root_teams_only = true
   summary_only    = true
